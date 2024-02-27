4c26106190540da7124606e68fae52ce5d2b4d55
mv0623 <156902469+mv0623@users.noreply.github.com>
2024-02-27 19:27:06 +0100

    Update Pintaexamen.java

--- src/pintaexamen/Pintaexamen.java
+++ src/pintaexamen/Pintaexamen.java
@@ -4,6 +4,7 @@ public class Pintaexamen {
 	public static void main(String[] args) {
 		for (int i=0; i<10; i++) {
 			System.out.printf("\uD83D\uDE00");
+			System.out.printf("Mascie");
 			}
 		}
 	}
